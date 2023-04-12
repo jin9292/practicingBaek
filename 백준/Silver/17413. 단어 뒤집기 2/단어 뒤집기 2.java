2301321ec804a31c8ff60a8c450aefb0f991617b
jin9292 <116279600+jin9292@users.noreply.github.com>
2023-04-12 20:22:23 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 240 ms, Memory: 30796 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -24,11 +24,12 @@ public class Main {
             if(isTag){ //tag임
                 answer.append(inputArray[i]);
             } else  { //tag가 아님
-                if (" ".equals(String.valueOf(inputArray[i]))) {
+                //주의할 점 : String과 Char을 equals method로 비교하기 위해 char -> String으로 변환 필요!
+                if (" ".equals(String.valueOf(inputArray[i]))) { //" "이면 그 전까지의 문자열 뒤집고 공백을 더해서 answer에 추가
                     stringBuilder.reverse();
                     answer.append(stringBuilder+" ");
                     stringBuilder.setLength(0);
-                } else  {
+                } else  {   //문자열 뒤집기 위해 추가
                     stringBuilder.append(inputArray[i]);
                 }
             }
@@ -38,7 +39,7 @@ public class Main {
                 stringBuilder.setLength(0); //문자열을 입력받이 뒤집기 위해 문자열 초기화
             }
         }
-        if (stringBuilder != null) {
+        if (stringBuilder != null) {    //남은 문자열 있다면 뒤집어서 문자열에 추기
             stringBuilder.reverse();
             answer.append(stringBuilder);
         }
